87a5465d3969998151063ff509f5cd214d73e147
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-09-02 15:18:52 +0200

    initial commit + added navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>About me</li>
+            <li>Experience</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact me</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
