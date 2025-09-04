62830caf114041791d581e1245b8492e3044ce4b
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-09-04 22:15:47 +0200

    added about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import profileImg from './assets/ProfileImg.jpg'
+import './App.scss'
+import AboutMe from './components/AboutMe/AboutMe'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <Hero />
+
+      <main className="intro container">
+        <div className="intro-left">
+          <section id='hero'><Hero/></section>
+          <section id='about'><AboutMe/></section>
+        </div>
+
+        <aside className='intro-right'>
+          <img className='hero-photo' src={profileImg} alt="" />
+        </aside>
+        
+      </main>
+     
     </div>
   )
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,16 +5,10 @@ import profileImg from '../../assets/ProfileImg.jpg'
 const Hero = () => {
   return (
     <div className='hero container'>
-        <div className="content-side">
-            <a href="">← Scroll to see more</a>
-        </div>
-
         <div className="hero-content">
             <h1 className='title'>Hi I'm <span className="accent">Edvaldo</span>.</h1>
             <p className="caption"><span className="accent">Software Developer</span> based in <span className='accent'>Regensburg, Germany</span></p> 
         </div>
-
-        <div className="hero-photo"><img src={profileImg} alt="" /></div>
 
     </div>
   )
