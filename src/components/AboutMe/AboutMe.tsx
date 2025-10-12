ccef8cdfca19ec4e7e9be21c7d87104f9eba3fdc
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-10-12 21:54:50 +0200

    adjusted About me text

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -6,8 +6,17 @@ const AboutMe = () => {
         <div className="aboutme-content">
             <h1 className='title'>About me</h1>
             <p className='caption'>
-              I am a Computer Engineering student in my final semester, passionate about Cloud Computing, DevOps, and Web Development. I discovered my love for coding during my studies and enjoy building projects that challenge me to learn and solve complex problems.<br/>
-               I have gained hands-on experience as a working student in DevOps, QA, and AI-driven projects at Infineon and Intive. In addition, I enjoy creating personal projects such as web applications and AI-driven prototypes that allow me to explore new technologies. Outside of tech, I am a passionate basketball player who values teamwork and continuous improvement, qualities that also shape how I approach my work.
+              I'm a Computer Engineering student in my final semester, passionate about Cloud Computing,
+              DevOps, and Web Development. During my studies, I discovered my enthusiasm for coding
+              and quickly realized how much I enjoy turning ideas into real, functional projects.<br/>
+
+              Through my work as a working student at Infineon and Intive, I have gained hands-on experience
+              in DevOps, QA, and AI-driven projects. This includes building deployment pipelines and contributing
+              to machine learning applications. In my personal projects, I also like to explore new technologies
+              to bring creative ideas to life.<br/>
+
+              Outside of tech, I'm a passionate basketball player who enjoys all kinds of athletic activities.
+              The teamwork, discipline, and focus it requires are the same values I bring into my professional work life.
             </p>
         </div>
         
